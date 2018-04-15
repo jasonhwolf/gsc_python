3759912ad6193bb90d9ef1439fbb9f7e8c7b7dee
jasonhwolf <jasonhwolf@gmail.com>
2018-04-15 15:26:31 -0700

    Adding Google webmaster file

--- GSC_API_v1.py
+++ GSC_API_v1.py
@@ -2,6 +2,7 @@
 # git remote add origin https://github.com/jasonhwolf/gsc_python.git
 # git commit -m "insert my comments here"
 # git push -u origin master
+# git log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
 
 print("This is a print")
 
