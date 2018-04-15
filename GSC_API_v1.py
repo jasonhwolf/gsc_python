13755be2cfa71469e5615164d2777d14e8751216
jasonhwolf <jasonhwolf@gmail.com>
2018-04-15 13:37:39 -0700

    More commits, nothing real

--- GSC_API_v1.py
+++ GSC_API_v1.py
@@ -1,4 +1,7 @@
 #This will be a python API workbook, someday.
+# git remote add origin https://github.com/jasonhwolf/gsc_python.git
+# git commit -m "insert my comments here"
+# git push -u origin master
 
 print("This is a print")
 
