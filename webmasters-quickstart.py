2d6d344b1a7c8dcd730c98aa6560bbc50fbc5190
jasonhwolf <jasonhwolf@gmail.com>
2018-04-25 08:43:13 -0700

    Adding JSON data file, instead of hardcoded

--- webmasters-quickstart.py
+++ webmasters-quickstart.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
+#python3 webmasters-quickstart.py
+
 import httplib2
+import json	
 
 from apiclient import errors
 from apiclient.discovery import build
@@ -8,8 +11,11 @@ from oauth2client.client import OAuth2WebServerFlow
 
 
 # Copy your credentials from the console
-CLIENT_ID = 'YOUR_CLIENT_ID'
-CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
+json_data = json.load(open('/Users/jasonwolf/gsc_python/client_secret_PS.json'))
+
+CLIENT_ID = json_data["installed"]["client_id"]
+CLIENT_SECRET = json_data["installed"]["client_secret"]
+
 
 # Check https://developers.google.com/webmaster-tools/search-console-api-original/v3/ for all available scopes
 OAUTH_SCOPE = 'https://www.googleapis.com/auth/webmasters.readonly'
@@ -20,9 +26,10 @@ REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 # Run through the OAuth flow and retrieve credentials
 flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
 authorize_url = flow.step1_get_authorize_url()
-print 'Go to the following link in your browser: ' + authorize_url
-code = raw_input('Enter verification code: ').strip()
+print ('Go to the following link in your browser: ' + authorize_url)
+code = input('Enter verification code: ').strip()
 credentials = flow.step2_exchange(code)
+# 4/AACYyUOPCBdRLVQQOkS3D3X8UyzXNnqkRbrrtTbThsq9sD6-zVYRkJw
 
 # Create an httplib2.Http object and authorize it with our credentials
 http = httplib2.Http()
@@ -40,9 +47,14 @@ verified_sites_urls = [s['siteUrl'] for s in site_list['siteEntry']
 
 # Printing the URLs of all websites you are verified for.
 for site_url in verified_sites_urls:
-  print site_url
+  print (site_url)
   # Retrieve list of sitemaps submitted
   sitemaps = webmasters_service.sitemaps().list(siteUrl=site_url).execute()
   if 'sitemap' in sitemaps:
     sitemap_urls = [s['path'] for s in sitemaps['sitemap']]
-    print "  " + "\n  ".join(sitemap_urls)
+    print("  " + "\n  ".join(sitemap_urls))
+
+
+
+
+    
